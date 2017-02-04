266bb823552bd2488cb384fbfbb82dba66c40519
adolf <hedm0423@163.com>
2017-02-04 16:13:49 +0800

    add some demos in app/view.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
 from app import app
-
+from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello world"
+    return "Hello world"
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' %name
+
+@app.route('/request')
+def request():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' %user_agent
+
+from flask import make_response
+@app.route('/response')
+def response():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer','42')
+    return response
